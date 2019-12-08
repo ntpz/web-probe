304b7ae037dd220476d1a6a96dbbb6d792473e35
Vladislav Tolkachev <576243+ntpz@users.noreply.github.com>
2019-12-09 00:41:10 +0300

    JSONExtractor initial implementation

--- json-extractor.js
+++ json-extractor.js
@@ -1,7 +1,31 @@
 const jp = require('jsonpath');
 
-function fromJSON(selector, obj) {
-    return jp.query(obj, selector);
+
+function extractEntries(spec, data) {
+    return extractRows(spec.rows, data).map(row => extractRow(spec.fields, row));
 }
 
-module.exports = {fromJSON};
+function extractRows(rowSpec, data) {
+    return jp.query(data, rowSpec.selector)[0];
+}
+
+function extractRow(fieldSpecs, row) {
+    const fieldSpecsArray = Object.entries(fieldSpecs);
+    const fieldValuesArray = fieldSpecsArray.map(
+        ([alias, rules]) => [alias, extractField(alias, rules, row)]
+    );
+    return fromEntries(fieldValuesArray);
+}
+
+function extractField(alias, rules, row) {
+    return jp.query(row, rules.selector)[0];
+}
+
+function fromEntries(entries) {
+    return Array.from(entries).reduce((obj, [key, value]) => {
+        obj[key] = value
+        return obj
+    }, {});
+}
+
+module.exports = { extractEntries };
--- test/json-extractor.test.js
+++ test/json-extractor.test.js
@@ -3,11 +3,28 @@ const assertArrays = require('chai-arrays');
 chai.use(assertArrays);
 const expect = require("chai").expect;
 
-const {fromJSON} = require('../json-extractor.js');
+const { extractEntries } = require('../json-extractor.js');
 
 
 describe("JSON extractor", () => {
-    it("Shuld extract scalar value from json document", () => {
-        expect(fromJSON('$.answer', {answer: 42})).to.eql([42]);    
+    it('Should extract entries according to rules', () => {
+        const data = {
+            persons: [
+                { name: 'Alice', age: 42 },
+                { name: 'Bob', age: 43, alive: false }
+            ]
+        };
+        const spec = {
+            rows: { selector: '$.persons' },
+            fields: {
+                name: { selector: '$.name' },
+                age: { selector: '$.age' }
+            }
+        };
+        const result = extractEntries(spec, data);
+        expect(result).to.be.array().ofSize(2);
+        expect(result).to.deep.include({name: 'Alice', age: 42});
+        expect(result).to.deep.include( {name: 'Bob', age: 43});
+
     });
 });
