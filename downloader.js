d3be66f80ab39441a15bcb9c8abd436aba2bcd7b
Vladislav Tolkachev <576243+ntpz@users.noreply.github.com>
2019-12-19 18:55:28 +0300

    Reformat code

--- downloader.js
+++ downloader.js
@@ -1,18 +1,18 @@
-const { isJSONresponse } = require("./utils");
+const { isJSONresponse } = require('./utils')
 
-const fetch = require("node-fetch");
+const fetch = require('node-fetch')
 
 async function download(url, options) {
-  let body;
-  const res = await fetch(url, options);
-  if (isJSONresponse(res)) {
-    body = await res.json();
-  } else {
-    body = await res.text();
-  }
-  return body;
+    let body
+    const res = await fetch(url, options)
+    if (isJSONresponse(res)) {
+        body = await res.json()
+    } else {
+        body = await res.text()
+    }
+    return body
 }
 
 module.exports = {
-  download
-};
+    download,
+}
